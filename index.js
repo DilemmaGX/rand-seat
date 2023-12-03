7cfb25b924d217b51aae4abd84f5697dfacffd99
官方小傲娇 <111679857+DilemmaGX@users.noreply.github.com>
2023-12-04 00:27:37 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {  
     const generateBtn = document.getElementById('generate-btn');  
     const resultDiv = document.getElementById('result');  
-    const defaultJson = JSON.parse(localStorage.getItem('./lib/default.json')); // Assume that the json data is stored in localStorage under the key 'default.json'  
-    const list = defaultJson || []; // If localStorage does not have 'default.json', use an empty array as a fallback.  
-    let sortedList = list.slice().sort(() => Math.random() - 0.5); // Shuffle the array randomly. Sort it in ascending order.  
-    
+    let sortedList = [];  
+    fetch('./lib/default.json') // Fetch the JSON data from the specified path.  
+        .then(response => response.json()) // Parse the response as JSON.  
+        .then(data => {  
+            sortedList = data.slice().sort(() => Math.random() - 0.5); // Shuffle the array randomly. Sort it in ascending order.  
+        })  
+        .catch(error => console.error('Error:', error)); // Catch any errors that occur during the fetching process.  
     generateBtn.addEventListener('click', function () {  
         generateBtn.style.display = 'none'; // Hide the button after clicking it.  
         resultDiv.textContent = ''; // Clear the result div.  
-    
         for (let i = 0; i < sortedList.length; i++) { // Loop through the shuffled array and display each item in the result div.  
             const item = sortedList[i];  
             const itemEl = document.createElement('div'); // Create a new div for each item.  
             itemEl.textContent = `${i + 1}. ${item}`; // Add the item with its index to the div's text content.  
             itemEl.style.marginBottom = '10px'; // Add some space between each item.  
             resultDiv.appendChild(itemEl); // Append the item's div to the result div.  
-        }  });  });
+        }  
+    });  
+});
