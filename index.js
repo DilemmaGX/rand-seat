a3a8e11ac1c8ab7bcdd609b2a36a0e095ebcb89f
官方小傲娇 <111679857+DilemmaGX@users.noreply.github.com>
2023-12-03 22:04:21 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+document.getElementById('sortButton').addEventListener('click', function() {  
+    fetch('default.json')  
+        .then(response => response.json())  
+        .then(data => {  
+            data = JSON.parse(JSON.stringify(data)); // 防止改变原对象  
+            data.sort(() => Math.random() - 0.5); // 随机排序数组  
+            updateDisplay(data);  
+        })  
+        .catch(error => console.error('Error:', error));  
+});  
+  
+function updateDisplay(data) {  
+    const content = document.getElementById('content');  
+    content.innerHTML = '';  
+    data.forEach(item => {  
+        const card = document.createElement('div');  
+        card.className = 'card';  
+        card.textContent = JSON.stringify(item);  
+        content.appendChild(card);  
+    });  
+}
