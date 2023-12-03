04a419cca303c3b548798cf5e38030199964b27c
官方小傲娇 <111679857+DilemmaGX@users.noreply.github.com>
2023-12-03 22:16:24 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 document.getElementById('sortButton').addEventListener('click', function() {  
-    fetch('./lib/default.js')
+    fetch('default.json')  
         .then(response => response.json())  
         .then(data => {  
-            data = JSON.parse(JSON.stringify(data));
-            data.sort(() => Math.random() - 0.5);
+            data = JSON.parse(JSON.stringify(data)); // 防止改变原对象  
+            data.sort(() => Math.random() - 0.5); // 随机排序数组  
             updateDisplay(data);  
         })  
         .catch(error => console.error('Error:', error));  
@@ -12,31 +12,10 @@ document.getElementById('sortButton').addEventListener('click', function() {
 function updateDisplay(data) {  
     const content = document.getElementById('content');  
     content.innerHTML = '';  
-    let i = 1;  
     data.forEach(item => {  
         const card = document.createElement('div');  
         card.className = 'card';  
-        card.textContent = i + ": " + item;
+        card.textContent = JSON.stringify(item);  
         content.appendChild(card);  
-        i++;  
     });  
-    showSuccessMessage();
-}  
-  
-function showSuccessMessage() {  
-    const successMessage = document.getElementById('success-message');  
-    successMessage.textContent = '成功！';
-    successMessage.style.color = 'green';
-    successMessage.style.backgroundColor = 'lightgreen';
-    successMessage.style.borderRadius = '10px';
-    successMessage.style.boxShadow = '0px 0px 10px rgba(0, 0, 0, 0.1)';
-    successMessage.style.padding = '20px';
-    successMessage.style.marginTop = '30px'; 
-    successMessage.style.display = 'block';
-    setTimeout(() => {
-        successMessage.style.opacity = '0';  
-        setTimeout(() => {  
-            successMessage.style.display = 'none';
-        }, 300);  
-    }, 3000);
 }
