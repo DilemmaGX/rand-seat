d3eb4252dd9c6f2b00d9438b2230454e9b75bac5
官方小傲娇 <111679857+DilemmaGX@users.noreply.github.com>
2023-12-03 22:25:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-document.getElementById('sortButton').addEventListener('click', function() {  
-    fetch('./lib/default.json')  
-        .then(response => response.json())  
-        .then(data => {  
+document.getElementById('sortButton').addEventListener('click', function() {
+    fetch('./lib/default.json')
+        .then(response => response.json())
+        .then(data => {
             data = JSON.parse(JSON.stringify(data)); // 防止改变原对象  
             data.sort(() => Math.random() - 0.5); // 随机排序数组  
-            updateDisplay(data);  
-        })  
-        .catch(error => console.error('Error:', error));  
-});  
-  
-function updateDisplay(data) {  
-    const content = document.getElementById('content');  
-    content.innerHTML = '';  
-    data.forEach(item => {  
-        const card = document.createElement('div');  
-        card.className = 'card';  
-        card.textContent = JSON.stringify(item);  
-        content.appendChild(card);  
-    });  
+            updateDisplay(data);
+        })
+        .catch(error => console.error('Error:', error));
+});
+function updateDisplay(data) {
+    const content = document.getElementById('content');
+    content.innerHTML = '';
+    data.forEach(item => {
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.textContent = JSON.stringify(item).replace(/\"/g, ""); // 去除引号
+        content.appendChild(card);
+    });
 }
